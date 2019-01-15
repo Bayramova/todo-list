f13e256d2344d1f9bbba5829e1e04b0c68b6bafc
Sabina <sb.sabina00@gmail.com>
2019-01-16 00:01:04 +0300

    style the list

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-const addButton = document.querySelector('#add');
-addButton.addEventListener('click', function () {
+document.getElementById('add')
+	.addEventListener('click', function () {
+		const li = createItem();
+	});
+
+
+function createItem () {
 	const li = document.createElement('li');
-	li.innerText = prompt();
-	const list = document.querySelector('#list');
-	list.appendChild(li);
-});
+	const inputValue = document.getElementById('input').value;
+	const text = document.createTextNode(inputValue);
+  	li.appendChild(text);
+	
+	if (inputValue === '') {
+		alert('You must write something');
+	}
+	else {
+		document.getElementById('list')
+		.appendChild(li);
+	}
+
+	document.getElementById('input').value = '';
+
+	const deleteButton = document.createElement('span');
+	const txt = document.createTextNode('\u00D7');
+	deleteButton.className = 'close';
+	deleteButton.appendChild(txt);
+	li.appendChild(deleteButton);
+	
+	deleteButton.addEventListener('click', deleteItem);
+
+	li.addEventListener('click', toggle);
+
+	return li;
+}
+
+function deleteItem (event) {
+	const li = event.target.parentNode;
+	const list = document.getElementById('list');
+	list.removeChild(li);
+}
+
+function toggle (event) {
+	const li = event.target;
+	li.style.textDecoration === 'line-through'
+			?li.style.textDecoration = 'none'
+			:li.style.textDecoration = 'line-through';
+}
